ed01bd2384399614502d5e659f14d172c2ff30b6
Thaíse Alves <thaise.alves@teamsoft.com.br>
2025-04-12 20:28:33 -0300

    Initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/com/diario/diariopessoal/DiariopessoalApplication.java
@@ -0,0 +1,13 @@
+package com.diario.diariopessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiariopessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiariopessoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diario/diariopessoal/DiariopessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diario.diariopessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiariopessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
